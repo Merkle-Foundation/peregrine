47cb8a86703c65d6eabfee5a342fb15495c977cd
Erick <erick.arce10@gmail.com>
2020-12-19 18:22:57 -0500

    Message broadcasting to multiple verticles

--- peregrine-statuspage/src/main/java/org/merkle/peregrine/statuspage/SimpleEntry.java
+++ peregrine-statuspage/src/main/java/org/merkle/peregrine/statuspage/SimpleEntry.java
@@ -1,6 +1,5 @@
 package org.merkle.peregrine.statuspage;
 
-import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import org.merkle.peregrine.statuspage.discord.DiscordConsumer;
 import org.merkle.peregrine.statuspage.slack.SlackConsumer;
@@ -10,13 +9,9 @@ public class SimpleEntry {
     public static void main(String[] args) {
 
         final Vertx vertx = Vertx.vertx();
+
         vertx.deployVerticle(new SlackConsumer());
         vertx.deployVerticle(new DiscordConsumer());
-
-        vertx.deployVerticle(StatuspageHttpServerV1.class.getName(), new DeploymentOptions()
-            .setWorker(true)
-            .setInstances(2), event -> {
-
-        });
+        vertx.deployVerticle(new StatuspageHttpServerV1());
     }
 }
