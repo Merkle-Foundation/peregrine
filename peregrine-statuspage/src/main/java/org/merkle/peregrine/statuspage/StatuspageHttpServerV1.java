5b546d6a23b329dcbb444aa95c58d1fbe8defea6
Erick <erick.arce10@gmail.com>
2021-01-05 23:41:58 -0500

    Message broadcasting to multiple verticles

--- peregrine-statuspage/src/main/java/org/merkle/peregrine/statuspage/StatuspageHttpServerV1.java
+++ peregrine-statuspage/src/main/java/org/merkle/peregrine/statuspage/StatuspageHttpServerV1.java
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 import static org.merkle.peregrine.statuspage.Routes.PEREGRINE_STATUS_V1;
 import static org.merkle.peregrine.statuspage.Routes.PUBLISH_STATUS_V1;
@@ -22,14 +22,22 @@ public class StatuspageHttpServerV1 extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         final HttpServer httpServer = vertx.createHttpServer();
+        final HttpServer httpsServer = vertx.createHttpServer();
 
         final Router router = Router.router(vertx);
         router.route(HttpMethod.POST, PUBLISH_STATUS_V1).handler(this::statusPubHandler);
+        router.route(HttpMethod.GET, PUBLISH_STATUS_V1).handler(this::statusPubHandler);
+        router.route(HttpMethod.GET, PUBLISH_STATUS_V1 + "/*").handler(this::statusPubHandler);
         router.route(HttpMethod.GET, PEREGRINE_STATUS_V1).handler(this::peregrineUp);
+
 
         httpServer
                 .requestHandler(router)
-                .listen(8122);
+                .listen(80);
+
+        httpsServer
+                .requestHandler(router)
+                .listen(443);
     }
 
     private void peregrineUp(final RoutingContext ctx) {
@@ -40,6 +48,8 @@ public class StatuspageHttpServerV1 extends AbstractVerticle {
     }
 
     private void statusPubHandler(final RoutingContext ctx) {
+        System.out.println("Received request at: " + new Date());
+
         ctx.request().bodyHandler(handler -> {
             try {
                 final byte[] bytes = handler.getBytes();
